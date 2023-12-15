2486c572665332234ee2f2a7c8f724c49589fb55
monique <moniquejp16@gmail.com>
2023-12-15 18:57:53 -0300

    feat: creat page home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,20 +1,8 @@
 
-import React, { useContext, useEffect, useState } from 'react'
-import styled from 'styled-components'
-import { AuthContext } from '../context/authContext';
 
 export default function Home() {
-  
-    const {products, setProducts } = useContext(AuthContext);
-   
-    useEffect(() => {
-        
-    }, []);
 
-    return (
-        <>
-        ola tudo mundo
-        </>
-    )
 };
+
+
 
