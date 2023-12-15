ff222180e5e8a38679429b269298348b8301f7ff
monique <moniquejp16@gmail.com>
2023-12-15 14:46:16 -0300

    fix: atualization contex

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext } from "react";
 export const AuthContext = createContext({})
 
 export default function AuthProvider({ children }) {
-
+const [products, setProducts] 
     return (
         <AuthContext.Provider value={{
 
