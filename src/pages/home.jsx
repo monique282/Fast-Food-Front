3d01d03dbd17a304031a63846cff321ea72f2ca8
monique <moniquejp16@gmail.com>
2023-12-15 19:01:01 -0300

    feat: creat useEffect

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,22 @@
-
+import axios from "axios";
+import { AuthContext } from '../context/authContext';
+import React, { useContext, useEffect } from 'react'
 
 export default function Home() {
 
+    const { products, setProducts } = useContext(AuthContext);
+
+    const url = `${import.meta.env.VITE_API_URL}/home`;
+
+    useEffect(() => {
+        const promise = axios.get(url);
+        promise.then(response => {
+            setProducts(response.data);
+        })
+        promise.catch(err => {
+            console.log(err.response);
+        });
+    }, []);
 };
 
 
