3cc87b2c1fda7fd94192b51f187c37d908a873dd
monique <moniquejp16@gmail.com>
2023-12-15 20:17:12 -0300

    fix: removal of the rome function header

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -28,14 +28,6 @@ export default function Home() {
 
     return (
         <All>
-            <Header>
-                <SloganName>Fest Food</SloganName>
-                <Control>
-                    <Requests>Pedidos</Requests>
-                    <Kitchen>Cozinha</Kitchen>
-                    <Withdrawal>Retirada</Withdrawal>
-                </Control>
-            </Header>
             <Welcome>Seja bem Vindo(a)!</Welcome>
             <Search placeholder='O que voê procura?' type="text" value={search} onChange={e => setSearch(e.target.value)} ></Search>
             <Products>
@@ -66,61 +58,6 @@ const All = styled.div`
     width: 100%;
     height: 100%;
     background-color: #FFFFFF;
-`
-const Header = styled.div`
-    width: 100%;
-    height: 50px;
-    background-color: #2E5D15;
-    display: flex;
-    align-items: center;     
-`
-const SloganName = styled.div`
-    width: 180px;
-    height: 60px;
-    font-family: "Varela Round";
-    font-size: 24px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: white;
-    margin-right: 50%;
-`
-const Control = styled.div`
-    width: 300px;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
-const Requests = styled.div`
-    width: 180px;
-    height: 60px;
-    font-family: "Varela Round";
-    font-size: 15px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: white;
-`
-const Kitchen = styled.div`
-    width: 180px;
-    height: 60px;
-    font-family: "Varela Round";
-    font-size: 15px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: white; 
-`
-const Withdrawal = styled.div`
-    width: 180px;
-    height: 60px;
-    font-family: "Varela Round";
-    font-size: 15px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: white;
 `
 const Welcome = styled.div`
     width: 266px;
