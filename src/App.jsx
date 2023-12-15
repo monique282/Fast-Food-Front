23087a9deda5a8c52f8eca5cf538557bcc679ddf
monique <moniquejp16@gmail.com>
2023-12-15 20:14:34 -0300

    fix:redirecting home page

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <AuthProvider>
         <Header />
         <Routes>
-          <Route path="/home" element={<Home />} >
+          <Route path="/" element={<Home />} >
           </Route>
         </Routes>
       </AuthProvider>
