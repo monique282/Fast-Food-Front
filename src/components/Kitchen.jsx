e7c460cc6d59a92cb148098f97e3eb15e34c7830
monique <moniquejp16@gmail.com>
2023-12-16 15:29:18 -0300

    feat: division between ordering and preparing

--- src/components/Kitchen.jsx
+++ src/components/Kitchen.jsx
@@ -5,7 +5,14 @@ export default function Kitchen() {
 
     return (
         <All>
-            cozinha
+            <Preparing>
+                <p>Preparando:</p>
+              
+            </Preparing>
+            <Sidebar></Sidebar>
+            <Ready>
+                <p>Pronto:</p>
+            </Ready>
         </All>
     )
 };
@@ -14,4 +21,39 @@ const All = styled.div`
     width: 100%;
     height: 100%;
     background-color: #FFFFFF;
+    display: flex;
+    
+`
+const Preparing = styled.div`
+    width: 50%;
+    height: 100%;
+    background-color: red;
+    margin-top: 7%;
+    margin-left: 12%;
+        p{
+        font-family: "Varela Round";
+        font-size: 24px;
+        color: black;
+        font-weight: bold;
+        }
+`
+const Sidebar = styled.div`
+    width: 1px;
+    background-color: black;
+    color: #ffffff;
+    padding: 2px;
+    margin-top: 7%;
+`
+const Ready = styled.div`
+    width: 50%;
+    height: 100%;
+    background-color: #1411ba;
+    margin-top: 7%;
+    margin-left: 12%;
+        p{
+        font-family: "Varela Round";
+        font-size: 24px;
+        color: black;
+        font-weight: bold;
+        }
 `
