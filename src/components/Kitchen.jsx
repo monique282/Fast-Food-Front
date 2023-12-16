f0d5585ebd50265177ede0606c708322fb1afc01
monique <moniquejp16@gmail.com>
2023-12-16 04:44:29 -0300

    feat: creating the base configuration kitchen folder

---
+++ src/components/Kitchen.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export default function Kitchen() {
+
+
+    return (
+        <All>
+            cozinha
+        </All>
+    )
+};
+
+const All = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: #FFFFFF;
+`
