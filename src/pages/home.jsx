c2c8b58bf1f268ad7177b45387b02cd37eb7cbe0
monique <moniquejp16@gmail.com>
2023-12-18 04:00:42 -0300

    product page logic

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,12 +6,21 @@ import Combo from '../assets/images/Combo.jpeg';
 import dessert from '../assets/images/dessert.jpeg';
 import drinks from '../assets/images/drinks.png'
 import follow from '../assets/images/follow.jpg'
+import snack from '../assets/images/snacks.jpeg'
 
 
 export default function Home() {
 
     const { products, setProducts } = useContext(AuthContext);
-    const [search, setSearch] = useState()
+    const [search, setSearch] = useState([]);
+    const [productFiltered, setProductFiltered] = useState(false);
+    const [snacks, setSnacks] = useState([]);
+    const [follo, setFollow] = useState([]);
+    const [drink, setDrink] = useState([]);
+    const [desser, setDesert] = useState([]);
+    const [comboo, setCombo] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState();
+    const [searchResults, setSearchResults] = useState([]);
 
     const url = `${import.meta.env.VITE_API_URL}/home`;
 
@@ -19,13 +28,43 @@ export default function Home() {
         const promise = axios.get(url);
         promise.then(response => {
             setProducts(response.data);
+            const follo = response.data.filter(product => product.category === 'FOLLOW');
+            setFollow(follo);
+            const snacks = response.data.filter(product => product.category === 'SNACKS');
+            setSnacks(snacks);
+            const drink = response.data.filter(product => product.category === 'DRINK');
+            setDrink(drink);
+            const dessert = response.data.filter(product => product.category === 'DESSERT');
+            setDesert(dessert);
+            const comboo = response.data.filter(product => product.category === 'COMBOO');
+            setCombo(comboo);
         })
         promise.catch(err => {
             console.log(err.response);
         });
+
     }, []);
 
-    console.log(products)
+    function ShowSpecific(parament) {
+        setProductFiltered(true)
+        setSelectedCategory();
+        setSelectedCategory(parament)
+    };
+
+    function handleSearch(query) {
+        setSearch(query);
+        const filteredResults = products.filter((product) => {
+            const lowercaseQuery = query.toLowerCase();
+            const lowercaseName = product.name.toLowerCase();
+            const stringifiedId = String(product.id);
+            return lowercaseName.includes(lowercaseQuery) || stringifiedId.includes(lowercaseQuery);
+        });
+
+        setSearchResults(filteredResults);
+        setProductFiltered(query.length > 0);
+        setSelectedCategory();
+    };
+
     if (products.length === 0) {
         return (
             <>carregando</>
@@ -34,34 +73,113 @@ export default function Home() {
         return (
             <All>
                 <Welcome>Seja bem Vindo(a)!</Welcome>
-                <Search placeholder='O que voê procura?' type="text" value={search} onChange={e => setSearch(e.target.value)} ></Search>
+                <Search placeholder='O que voê procura?'
+                    type="text"
+                    value={search}
+                    onChange={e => handleSearch(e.target.value)} >
+                </Search>
                 <Products>
                     <Title>Categorias</Title>
                     <Subtitle>Navegue por categorais</Subtitle>
                     <Categories>
-                        <Box>
+                        <Box onClick={() => ShowSpecific("COMBOO")}>
                             <img src={Combo} alt="" />
                             <p style={{ fontWeight: "bold" }}>Combos</p>
                         </Box>
-                        <Box>
+                        <Box onClick={() => ShowSpecific("SNACKS")}>
+                            <img src={snack} alt="" />
+                            <p style={{ fontWeight: "bold" }}>Lanches</p>
+                        </Box>
+                        <Box onClick={() => ShowSpecific("FOLLOW")}>
                             <img src={follow} alt="" />
-                            <p style={{ marginTop: "-5px", fontWeight: "bold" }}>Acompanhamentos</p></Box>
-                        <Box>
+                            <p style={{ marginTop: "-5px", fontWeight: "bold" }}>Acompanhamentos</p>
+                        </Box>
+                        <Box onClick={() => ShowSpecific("DRINK")}>
                             <img src={drinks} alt="" />
-                            <p style={{ fontWeight: "bold" }}>Bebidas</p></Box>
-                        <Box>
+                            <p style={{ fontWeight: "bold" }}>Bebidas</p>
+                        </Box>
+                        <Box onClick={() => ShowSpecific("DESSERT")}>
                             <img src={dessert} alt="" />
-                            <p style={{ fontWeight: "bold" }}>Sobremesas</p></Box>
+                            <p style={{ fontWeight: "bold" }}>Sobremesas</p>
+                        </Box>
                     </Categories>
                     <Title>Produtos</Title>
                     <Subtitle>Selecione um produto para adicioar ao seu pedido</Subtitle>
                     <Menu>
-                        <ProductBox>
-                            <img src={products[1].image} alt="" />
-                            <h1>{products[1].name}</h1>
-                            <h2>{products[1].description}</h2>
-                            <p>R$ {products[1].price.toFixed(2)}</p>
-                        </ProductBox>
+                        {(!productFiltered) && (
+                            snacks.map((main) => (
+                                <ProductBox key={main.id}>
+                                    <img src={main.image} alt="" />
+                                    <h1>{main.name}</h1>
+                                    <h2>{main.description}</h2>
+                                    <p>R$ {main.price.toFixed(2)}</p>
+                                </ProductBox>
+                            ))
+                        )}
+                        {productFiltered === true && (
+                            <>
+                                {selectedCategory === "COMBOO" && (
+                                    comboo.map((main) => (
+                                        <ProductBox key={main.id}>
+                                            <img src={main.image} alt="" />
+                                            <h1>{main.name}</h1>
+                                            <h2>{main.description}</h2>
+                                            <p>R$ {main.price.toFixed(2)}</p>
+                                        </ProductBox>
+                                    ))
+                                )}
+                                {selectedCategory === "FOLLOW" && (
+                                    follo.map((main) => (
+                                        <ProductBox key={main.id}>
+                                            <img src={main.image} alt="" />
+                                            <h1>{main.name}</h1>
+                                            <h2>{main.description}</h2>
+                                            <p>R$ {main.price.toFixed(2)}</p>
+                                        </ProductBox>
+                                    ))
+                                )}
+                                {selectedCategory === "DESSERT" && (
+                                    desser.map((main) => (
+                                        <ProductBox key={main.id}>
+                                            <img src={main.image} alt="" />
+                                            <h1>{main.name}</h1>
+                                            <h2>{main.description}</h2>
+                                            <p>R$ {main.price.toFixed(2)}</p>
+                                        </ProductBox>
+                                    ))
+                                )}
+                                {selectedCategory === "SNACKS" && (
+                                    snacks.map((main) => (
+                                        <ProductBox key={main.id}>
+                                            <img src={main.image} alt="" />
+                                            <h1>{main.name}</h1>
+                                            <h2>{main.description}</h2>
+                                            <p>R$ {main.price.toFixed(2)}</p>
+                                        </ProductBox>
+                                    ))
+                                )}
+                                {selectedCategory === "DRINK" && (
+                                    drink.map((main) => (
+                                        <ProductBox key={main.id}>
+                                            <img src={main.image} alt="" />
+                                            <h1>{main.name}</h1>
+                                            <h2>{main.description}</h2>
+                                            <p>R$ {main.price.toFixed(2)}</p>
+                                        </ProductBox>
+                                    ))
+                                )}
+                                {search.length !== 0 && (
+                                    searchResults.map((main) => (
+                                        <ProductBox key={main.id}>
+                                            <img src={main.image} alt="" />
+                                            <h1>{main.name}</h1>
+                                            <h2>{main.description}</h2>
+                                            <p>R$ {main.price.toFixed(2)}</p>
+                                        </ProductBox>
+                                    ))
+                                )}
+                            </>
+                        )}
                     </Menu>
                 </Products>
             </All >
@@ -148,12 +266,17 @@ const Box = styled.div`
         width: 100px;
         margin-left: 25%;
         margin-bottom: 3px;
+        z-index: 0
         }
 `
 const Menu = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
 `
 const ProductBox = styled.div`
-    width: 180px;
+    width: calc(20% - 2.4%);
+    max-width: 180px; 
     height: 220px;
     border: none;
     border-radius: 10px;
@@ -161,35 +284,35 @@ const ProductBox = styled.div`
     flex-direction: column;
     text-align: center;
     box-shadow: 0 1px 4px rgba(0,0,0,0.15);
-    margin-top: 2%;
-        img{
+    margin-top: 2.4%;
+    margin-left: 2.4%; 
+        img {
         width: 100px;
         margin-left: 23%;
         margin-bottom: 3px;
         margin-top: 10px;
         }
-        h1{
+        h1 {
         font-family: "Varela Round";
         font-size: 20px;
         color: black;
         margin-top: 5%;
         font-weight: bold;
         }
-        h2{
+        h2 {
         font-family: "Varela Round";
         font-size: 11px;
         color: black;
         margin-top: 5%;
         }
-        p{
+        p {
         font-family: "Varela Round";
         font-size: 17px;
         color: black;
-        margin-top: 5%; 
-        font-weight: bold; 
+        margin-top: 5%;
+        font-weight: bold;
         }
 `
-const sdv = styled.div`
-`
+
 
 
