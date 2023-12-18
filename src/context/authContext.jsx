3b3f1d466ed264c99035bf664e3afd6835a83b54
monique <moniquejp16@gmail.com>
2023-12-18 05:16:07 -0300

    feat: add counter

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -5,7 +5,8 @@ export const AuthContext = createContext({})
 export default function AuthProvider({ children }) {
     const [products, setProducts] = useState([]);
     const [id, setId] = useState();
-    const [showReview, setShowReview] = useState(false)
+    const [showReview, setShowReview] = useState(false);
+    const [counter, setCounter] = useState(1)
 
     return (
         <AuthContext.Provider value={{
