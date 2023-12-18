71ab902aeae2e2c2a41c438858b3712cb7c28417
monique <moniquejp16@gmail.com>
2023-12-18 04:38:59 -0300

    feat: adding the id

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -3,10 +3,12 @@ import React, { createContext, useState } from "react";
 export const AuthContext = createContext({})
 
 export default function AuthProvider({ children }) {
-const [products, setProducts] = useState([])
+    const [products, setProducts] = useState([]);
+    const [id, setId] = useState()
     return (
         <AuthContext.Provider value={{
-            products, setProducts
+            products, setProducts,
+            id, setId
         }}>
             {children}
         </AuthContext.Provider>
