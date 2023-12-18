2f590ea2e25a019581247a4051ad7ae96510159e
monique <moniquejp16@gmail.com>
2023-12-18 18:38:03 -0300

    feat: update order and remove order

--- src/components/Revision.jsx
+++ src/components/Revision.jsx
@@ -33,7 +33,6 @@ export default function Revision() {
             setBaconSelected(corresponding.followUp.some((item) => item.id === 1));
             setCheddarSelected(corresponding.followUp.some((item) => item.id === 2));
             setSauceSelected(corresponding.followUp.some((item) => item.id === 3));
-            // remover item
             const updatedOrder = order.filter((item) => item.ProductSpecific.id !== id);
             setOrder(updatedOrder);
         }
