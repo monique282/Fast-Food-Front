6312b1e91de0879e96919431d2490db4ca9b3d86
monique <moniquejp16@gmail.com>
2023-12-20 19:29:24 -0300

    feat: showing only ready orders from the last 2 hours

--- src/components/Kitchen.jsx
+++ src/components/Kitchen.jsx
@@ -6,45 +6,74 @@ import { HiOutlineX } from "react-icons/hi";
 import axios from "axios";
 
 export default function Kitchen() {
-  const {
-    products,
-    setProducts,
-    setId,
-    showReview,
-    setShowReview,
-    order,
-    showPayment,
-    setShowPayment,
-    setCode,
-    code,
-    setOrder,
-    showRequest,
-    setShowRequest,
-  } = useContext(AuthContext);
+  const { showRequest, setShowRequest, order } = useContext(AuthContext);
 
   const [notReadyRequests, setNotReadyRequests] = useState([]);
-  const [readyRequests, setReadyRequests] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [showOnly2hours, setShowOnly2hours] = useState([]);
 
   useEffect(() => {
-    const urlRequest = `${import.meta.env.VITE_API_URL}/request`;
+    function requisition(){
+        const urlRequest = `${import.meta.env.VITE_API_URL}/request`;
 
     const promise = axios.get(urlRequest);
     promise.then((response) => {
       setShowRequest(response.data);
-      filtering();
+      setNotReadyRequests(showRequest.filter((item) => item.ready === false));
+      setLoading(false);
+      theLastTwoHours(showRequest.filter((item) => item.ready === true));
+      console.log("aqui")
+    });
+    promise.catch((err) => {
+      console.log(err.response);
+      setLoading(false);
+    });
+    }
+    requisition(); 
+
+    const intervalId = setInterval(requisition, 30 * 1000);
+
+
+    return () => clearInterval(intervalId);
+  }, [order, loading]);
+
+
+  function theLastTwoHours(readyRequests) {
+    const twoHoursInMilliseconds = 2 * 60 * 60 * 1000;
+    const now = new Date();
+    const ordersInLastTwoHours = readyRequests.filter((pedido) => {
+      const dateCreation = new Date(pedido.createdAt);
+      return now - dateCreation <= twoHoursInMilliseconds;
+    });
+
+ const orderedByDate = ordersInLastTwoHours.sort((a, b) => {
+    const dataA = new Date(a.createdAt);
+    const dataB = new Date(b.createdAt);
+    return dataB - dataA;
+  });
+
+  setShowOnly2hours(orderedByDate);
+  }
+
+  function ready(code) {
+    console.log(code);
+    const urlRequest = `${import.meta.env.VITE_API_URL}/updateReady`;
+    const data = {
+      code,
+    };
+    const promise = axios.post(urlRequest, data);
+    promise.then((response) => {
+      setLoading(true);
     });
     promise.catch((err) => {
       console.log(err.response);
     });
-  }, [order]);
+  }
 
-  function filtering() {
-    setNotReadyRequests(showRequest.filter((item) => item.ready === false));
-    setReadyRequests(showRequest.filter((item) => item.ready === true));
-    console.log("preparando", notReadyRequests);
-    console.log(readyRequests);
-  };
-  
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <All>
       <Preparing>
@@ -64,7 +93,7 @@ export default function Kitchen() {
                   style={{ width: "30px", height: "30px", color: "#CF3C29" }}
                 />
               </Not>
-              <Ok>
+              <Ok onClick={() => ready(main.code)}>
                 <HiOutlineCheck
                   style={{ width: "30px", height: "30px", color: "#56A211" }}
                 />
@@ -76,8 +105,8 @@ export default function Kitchen() {
       <Sidebar></Sidebar>
       <Ready>
         <p>Pronto:</p>
-        {readyRequests.map((main) => (
-          <Order key={main.idR}>
+        {showOnly2hours.map((main) => (
+          <OrderReady key={main.idR}>
             <img src={main.image} alt="" />
             <NameCode>
               <h1>
@@ -92,7 +121,7 @@ export default function Kitchen() {
                 />
               </Not>
             </Butons>
-          </Order>
+          </OrderReady>
         ))}
       </Ready>
     </All>
@@ -109,7 +138,7 @@ const Preparing = styled.div`
   width: 50%;
   height: 100%;
   margin-top: 7%;
-  margin-left: 7%; 
+  margin-left: 7%;
   margin-bottom: 7%;
   p {
     font-family: "Varela Round";
@@ -126,6 +155,7 @@ const Sidebar = styled.div`
   margin-top: 7%;
   border-radius: 10px;
 `;
+
 const Ready = styled.div`
   width: 50%;
   height: 100%;
@@ -139,11 +169,29 @@ const Ready = styled.div`
     font-weight: bold;
   }
 `;
+
+const OrderReady = styled.div`
+  width: 90%;
+  height: 100%;
+  border: 1px solid #67ad68;
+  border-radius: 20px;
+  display: flex;
+  text-align: center;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.15);
+  margin-top: 5%;
+  img {
+    width: 100px;
+    height: 80px;
+    margin: 2%;
+    border-radius: 10px;
+    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.15);
+  }
+`;
 const Order = styled.div`
   width: 90%;
   height: 100%;
   border: none;
-  border-radius: 10px;
+  border-radius: 20px;
   display: flex;
   text-align: center;
   box-shadow: 0 1px 4px rgba(0, 0, 0, 0.15);
@@ -190,6 +238,7 @@ const Not = styled.div`
   align-items: center;
   background-color: #fae5e5;
 `;
+
 const Ok = styled.div`
   width: 50px;
   height: 50px;
