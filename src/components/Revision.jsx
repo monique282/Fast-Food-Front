825a7279d05c915c700c67936e9aa856fb6e6cde
monique <moniquejp16@gmail.com>
2024-01-22 16:02:25 -0300

    fix: componentization PurchaseSummaryRevision

--- src/components/Revision.jsx
+++ src/components/Revision.jsx
@@ -44,6 +44,7 @@ import ExitRevision from "../Return/revision/ExitRevision";
 import ProductOverviewRevision from "../Return/revision/ProductOverviewRevision";
 import AdditionalRevision from "../Return/revision/AdditionalRevision";
 import Observationrevision from "../Return/revision/Observationrevision";
+import PurchaseSummaryRevision from "../Return/revision/PurchaseSummaryRevision";
 
 export default function Revision() {
   const {
@@ -108,50 +109,54 @@ export default function Revision() {
             observationText={observationText}
             setObservationText={setObservationText}
           />
+          <PurchaseSummaryRevision
+            counter={counter}
+            ProductSpecific={ProductSpecific}
+            baconSelected={baconSelected}
+            cheddarSelected={cheddarSelected}
+            sauceSelected={sauceSelected}
+          />
 
-          <PurchaseSummary>
+          <DescriptionPrice>
+            <Summary>
+              {counter}x {ProductSpecific.name}
+            </Summary>
+            <PriceDescription>
+              R$ {(ProductSpecific.price * counter).toFixed(2)}
+            </PriceDescription>
+          </DescriptionPrice>
+          {baconSelected === true && (
             <DescriptionPrice>
-              <Summary>
-                {counter}x {ProductSpecific.name}
-              </Summary>
-              <PriceDescription>
-                R$ {(ProductSpecific.price * counter).toFixed(2)}
-              </PriceDescription>
+              <Summary>1x Bacon 10g</Summary>
+              <PriceDescription>R$ 1.00</PriceDescription>
             </DescriptionPrice>
-            {baconSelected === true && (
-              <DescriptionPrice>
-                <Summary>1x Bacon 10g</Summary>
-                <PriceDescription>R$ 1.00</PriceDescription>
-              </DescriptionPrice>
-            )}
-            {cheddarSelected === true && (
-              <DescriptionPrice>
-                <Summary>1x Chedar 10g</Summary>
-                <PriceDescription>R$ 1.00</PriceDescription>
-              </DescriptionPrice>
-            )}
-            {sauceSelected === true && (
-              <DescriptionPrice>
-                <Summary>1x Molho acompanhamento Barbecue</Summary>
-                <PriceDescription>R$ 1.00</PriceDescription>
-              </DescriptionPrice>
-            )}
-            <Divider></Divider>
-            <FinalValue>
-              <h1>Total do pedido:</h1>
-              <Amount>
-                R${" "}
-                {(
-                  ProductSpecific.price * counter +
-                  (baconSelected ? 1 : 0) +
-                  (cheddarSelected ? 1 : 0) +
-                  (sauceSelected ? 1 : 0)
-                ).toFixed(2)}
-              </Amount>
-            </FinalValue>
-          </PurchaseSummary>
+          )}
+          {cheddarSelected === true && (
+            <DescriptionPrice>
+              <Summary>1x Chedar 10g</Summary>
+              <PriceDescription>R$ 1.00</PriceDescription>
+            </DescriptionPrice>
+          )}
+          {sauceSelected === true && (
+            <DescriptionPrice>
+              <Summary>1x Molho acompanhamento Barbecue</Summary>
+              <PriceDescription>R$ 1.00</PriceDescription>
+            </DescriptionPrice>
+          )}
+          <Divider></Divider>
+          <FinalValue>
+            <h1>Total do pedido:</h1>
+            <Amount>
+              R${" "}
+              {(
+                ProductSpecific.price * counter +
+                (baconSelected ? 1 : 0) +
+                (cheddarSelected ? 1 : 0) +
+                (sauceSelected ? 1 : 0)
+              ).toFixed(2)}
+            </Amount>
+          </FinalValue>
 
-          
           <Finishing>
             <RemoveOrderFromList
               onClick={() => {
