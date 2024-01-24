9772bb35315c5bbf6f9416dc0cc01e8c16cd5eff
monique <moniquejp16@gmail.com>
2024-01-24 13:53:12 -0300

    fix: eslint fix SearchResultsTrue

--- src/Return/home/SearchResultsTrue.jsx
+++ src/Return/home/SearchResultsTrue.jsx
@@ -1,12 +1,20 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import ProcuctBoxHome from "./ProductBoxHome";
+import PropTypes from "prop-types";
+
 export default function SearchResultsTrue({
   searchResults,
   search,
   ordereIds,
   ShowProductDetails,
 }) {
+  SearchResultsTrue.propTypes = {
+    searchResults: PropTypes.func.isRequired,
+    search: PropTypes.func.isRequired,
+    ordereIds: PropTypes.func.isRequired,
+    ShowProductDetails: PropTypes.func.isRequired,
+  };
   return (
     <>
       {search.length !== 0 &&
