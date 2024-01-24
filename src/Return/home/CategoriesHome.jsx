058f61ab12f5b74c60410dae09dde702a5797ccc
monique <moniquejp16@gmail.com>
2024-01-24 13:30:29 -0300

    fix: eslint fix CategoriesHome

--- src/Return/home/CategoriesHome.jsx
+++ src/Return/home/CategoriesHome.jsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { Categories } from "../../assets/StylesPages/home";
 import BoxHome from "./BoxHome";
+import PropTypes from "prop-types";
+
 export default function CategoriesHome({
   setProductFiltered,
   setSelectedCategory,
@@ -11,43 +13,52 @@ export default function CategoriesHome({
   drinks,
   dessert,
 }) {
+  CategoriesHome.propTypes = {
+    setProductFiltered: PropTypes.func.isRequired,
+    setSelectedCategory: PropTypes.func.isRequired,
+    Combo: PropTypes.string.isRequired,
+    snack: PropTypes.string.isRequired,
+    follow: PropTypes.string.isRequired,
+    drinks: PropTypes.string.isRequired,
+    dessert: PropTypes.string.isRequired,
+  };
   return (
     <Categories>
-            <BoxHome
-              Imagem={Combo}
-              text={"Combo"}
-              productText={"COMBOO"}
-              setProductFiltered={setProductFiltered}
-              setSelectedCategory={setSelectedCategory}
-            />
-            <BoxHome
-              Imagem={snack}
-              text={"Lanches"}
-              productText={"SNACKS"}
-              setProductFiltered={setProductFiltered}
-              setSelectedCategory={setSelectedCategory}
-            />
-            <BoxHome
-              Imagem={follow}
-              text={"Acompanhamentos"}
-              productText={"FOLLOW"}
-              setProductFiltered={setProductFiltered}
-              setSelectedCategory={setSelectedCategory}
-            />
-            <BoxHome
-              Imagem={drinks}
-              text={"Bebidas"}
-              productText={"DRINK"}
-              setProductFiltered={setProductFiltered}
-              setSelectedCategory={setSelectedCategory}
-            />
-             <BoxHome
-              Imagem={dessert}
-              text={"Sobremesas"}
-              productText={"DESSERT"}
-              setProductFiltered={setProductFiltered}
-              setSelectedCategory={setSelectedCategory}
-            />
-          </Categories>
+      <BoxHome
+        Imagem={Combo}
+        text={"Combo"}
+        productText={"COMBOO"}
+        setProductFiltered={setProductFiltered}
+        setSelectedCategory={setSelectedCategory}
+      />
+      <BoxHome
+        Imagem={snack}
+        text={"Lanches"}
+        productText={"SNACKS"}
+        setProductFiltered={setProductFiltered}
+        setSelectedCategory={setSelectedCategory}
+      />
+      <BoxHome
+        Imagem={follow}
+        text={"Acompanhamentos"}
+        productText={"FOLLOW"}
+        setProductFiltered={setProductFiltered}
+        setSelectedCategory={setSelectedCategory}
+      />
+      <BoxHome
+        Imagem={drinks}
+        text={"Bebidas"}
+        productText={"DRINK"}
+        setProductFiltered={setProductFiltered}
+        setSelectedCategory={setSelectedCategory}
+      />
+      <BoxHome
+        Imagem={dessert}
+        text={"Sobremesas"}
+        productText={"DESSERT"}
+        setProductFiltered={setProductFiltered}
+        setSelectedCategory={setSelectedCategory}
+      />
+    </Categories>
   );
 }
