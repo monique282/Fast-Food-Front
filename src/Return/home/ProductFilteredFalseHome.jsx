0c811ceb5dfe345308c61b238c52e9cbaec5a2f5
monique <moniquejp16@gmail.com>
2024-01-24 13:40:06 -0300

    fix: eslint fix ProductFilteredFalseHome

--- src/Return/home/ProductFilteredFalseHome.jsx
+++ src/Return/home/ProductFilteredFalseHome.jsx
@@ -1,12 +1,21 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import ProcuctBoxHome from "./ProductBoxHome";
+import PropTypes from "prop-types";
+
 export default function ProductFilteredFalseHome({
   productFiltered,
   snacks,
   ordereIds,
   ShowProductDetails,
 }) {
+  ProductFilteredFalseHome.propTypes = {
+    productFiltered: PropTypes.func.isRequired,
+    snacks: PropTypes.func.isRequired,
+    main: PropTypes.array.isRequired,
+    ordereIds: PropTypes.func.isRequired,
+    ShowProductDetails: PropTypes.func.isRequired,
+  };
   return (
     <>
       {!productFiltered &&
