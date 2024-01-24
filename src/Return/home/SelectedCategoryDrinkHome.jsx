ff06d65c13027436519e2b0d2237001ea53fa9f3
monique <moniquejp16@gmail.com>
2024-01-24 13:56:20 -0300

    fix: eslint fix SelectedCategoryDrinkHome

--- src/Return/home/SelectedCategoryDrinkHome.jsx
+++ src/Return/home/SelectedCategoryDrinkHome.jsx
@@ -1,12 +1,20 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import ProcuctBoxHome from "./ProductBoxHome";
+import PropTypes from "prop-types";
+
 export default function SelectedCategoryDrinkHome({
   selectedCategory,
   drink,
   ordereIds,
   ShowProductDetails,
 }) {
+  SelectedCategoryDrinkHome.propTypes = {
+    selectedCategory: PropTypes.func.isRequired,
+    drink: PropTypes.array.isRequired,
+    ordereIds: PropTypes.func.isRequired,
+    ShowProductDetails: PropTypes.func.isRequired,
+  };
   return (
     <>
       {selectedCategory === "DRINK" &&
