08b843c7c88ae932ecc5f6b61debb3843d54d0e4
monique <moniquejp16@gmail.com>
2024-01-24 15:56:50 -0300

    fix: eslint fix Observationrevision

--- src/Return/revision/Observationrevision.jsx
+++ src/Return/revision/Observationrevision.jsx
@@ -1,9 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
 import { Observation, Search } from "../../assets/StylesPages/revision";
+import PropTypes from "prop-types";
 
-export default function Observationrevision(
+export default function Observationrevision({
   observationText,
-  setObservationText
-) {
+  setObservationText,
+}) {
+  Observationrevision.propTypes = {
+    setObservationText: PropTypes.func.isRequired,
+    observationText: PropTypes.string.isRequired,
+  };
   return (
     <Observation>
       <h1>Observações</h1>
